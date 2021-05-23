fc0c7a74c769d25b7f00d6c667edf26a175cd589
Paulo <pauloh16.pdr@gmail.com>
2021-05-23 03:36:44 -0300

    Corrigir problemas nas funcoes que convertem segundos em minutos e horas

--- src/utils/secondsToTime.ts
+++ src/utils/secondsToTime.ts
@@ -1,7 +1,8 @@
-export const secondsToTime = (seconds: number): string => {
-  const zeroLeft = (n: number) => Math.floor(n).toString().padStart(2, '0');
-  const min = zeroLeft((seconds / 60) % 60);
-  const sec = zeroLeft((seconds % 60) % 60);
+import { secondsToMinutes } from './secondsToMinutes';
+import { zeroLeft } from './zeroLeft';
 
-  return `${min}:${sec}`;
+export const secondsToTime = (seconds: number): string => {
+  const hours = zeroLeft(seconds / 3600);
+
+  return `${hours}:${secondsToMinutes(seconds)}`;
 };
